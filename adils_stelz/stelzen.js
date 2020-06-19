6327376d1519419e6ef94802d6f200814cd49c68
fence <fence@desu-mail.moe>
2020-06-19 13:07:02 +0200

    add adils_stelz

---
+++ adils_stelz/stelzen.js
@@ -0,0 +1,45 @@
+const diameter = 39.6;
+const diameter_ground = 30;
+const edge_width = 5;
+const height = 40;
+const indet_height = 10;
+const fn = 20;
+const hollow = true;
+const hollow_shit = 10;
+
+
+function main() {
+    let stelz = cylinder({
+        d1: diameter + edge_width,
+        d2: diameter + edge_width + diameter_ground,
+        h: height,
+        fn,
+    });
+
+    const socket = cylinder({
+        d: diameter, 
+        h: indet_height,
+        fn,
+    });
+
+    stelz = difference(stelz, socket);
+
+    if (hollow) {
+        const nom_nom = translate(
+            [0, 0, indet_height],
+            cylinder({
+                d1: diameter - hollow_shit,
+                d2: diameter + diameter_ground,
+                h: height - indet_height,
+                fn,
+            })
+        );
+
+        stelz = difference(stelz, nom_nom);
+    }
+    
+    return translate(
+        [0, 0, height], 
+        rotate([180, 0, 0], stelz)
+    );
+}
